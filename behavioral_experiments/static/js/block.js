96061d1ae1a3e1373444a7bc98c7c8430aedd467
Robert Hawkins <rxdh@stanford.edu>
2020-02-17 21:10:40 -0500

    Class-ify block, blockKind, blockMenu

--- behavioral_experiments/static/js/block.js
+++ behavioral_experiments/static/js/block.js
@@ -1,17 +1,23 @@
-// Wrappers for Matter Bodies that instantiate a particular BlockKind
-function Block(blockKind, x, y, rotated, testing_placement = false, x_index = null,  y_index = null) {
+var config = require('./display_config.js');
+var Matter = require('./matter.js');
 
+// Wrappers for Matter Bodies that instantiate a particular BlockKind
+class Block {
+  constructor(engine, blockKind, x, y, rotated,
+              testing_placement = false, x_index = null,  y_index = null) {
+
+    this.engine = engine;
     this.blockKind = blockKind;
     this.x_index = x_index;
     this.y_index = y_index;
     this.color = this.blockKind.blockColor;
 
     if (rotated) {
-        this.w = blockKind.h * sF;
-        this.h = blockKind.w * sF;
+        this.w = blockKind.h * config.sF;
+        this.h = blockKind.w * config.sF;
     } else {
-        this.w = blockKind.w * sF;
-        this.h = blockKind.h * sF;
+        this.w = blockKind.w * config.sF;
+        this.h = blockKind.h * config.sF;
     }
 
     //var options = blockKind.options;
@@ -19,67 +25,68 @@ function Block(blockKind, x, y, rotated, testing_placement = false, x_index = nu
     var options = {
         friction: 0.9,
         frictionStatic: 1.4,
-        //frictionAir: 0.07,
         density: 0.0035,
         restitution: 0.0015,
         sleepThreshold: 30
     }
     if (!testing_placement) {
-        this.originalX = x * worldScale;
-        this.originalY = y * worldScale;
+      this.originalX = x * config.worldScale;
+      this.originalY = y * config.worldScale;
+      
+      this.body = Matter.Bodies.rectangle(this.originalX, this.originalY,
+                                          this.w * config.worldScale,
+                                          this.h * config.worldScale, options);
+      Matter.World.add(this.engine.world, this.body);
+    } else {
+      this.test_body = Matter.Bodies.rectangle(
+        x * this.worldScale, 
+        y * this.worldScale,
+        this.w * this.worldScale * 0.85,
+        this.h * this.worldScale * 0.85
+      );
+      this.test_body.collisionFilter.category = 3;
+    }
+  }
+  
+  // Display the block
+  show (env) {
+    var pos = this.body.position;
+    var angle = this.body.angle;
 
-        this.body = Bodies.rectangle(this.originalX, this.originalY, this.w * worldScale, this.h * worldScale, options);
-        World.add(engine.world, this.body);
+    env.push(); //saves the current drawing style settings and transformations
+    env.rectMode(env.CENTER);
+    env.translate(pos.x / config.worldScale, pos.y / config.worldScale);
+    env.rotate(angle);
+    env.stroke([28,54,62]);
+    env.strokeWeight(2);
+    env.fill(this.color);
+    // if(this.body.isSleeping) {
+    //     env.fill();
+    // }
+
+    if (!config.chocolateBlocks) {
+      env.rect(0, 0, this.w, this.h);
     }
     else {
-        this.test_body = Bodies.rectangle(x * worldScale, y * worldScale, this.w * worldScale * 0.85, this.h * worldScale * 0.85);
-        this.test_body.collisionFilter.category = 3;
+      this.blockKind.drawChocolateBlocks(env);
     }
+    env.pop();
 
+  }
 
-    // Display the block
-    this.show = function (env) {
+  can_be_placed () {
+    var colliding_bodies = Matter.Query.region(this.engine.world.bodies, this.test_body.bounds);
+    return (colliding_bodies === undefined || colliding_bodies.length == 0)
+  }
 
-        var pos = this.body.position;
-        var angle = this.body.angle;
+  checkMotion () {
+    var xMove = Math.abs(this.body.position.x - this.originalX) > 10;
+    var yMove = Math.abs(this.body.position.y - this.originalY) > 10;
+    var rotated = Math.abs(this.body.angle) > 0.70
 
-        env.push(); //saves the current drawing style settings and transformations
-        env.rectMode(env.CENTER);
-        env.translate(pos.x / worldScale, pos.y / worldScale);
-        env.rotate(angle);
-        env.stroke([28,54,62]);
-        env.strokeWeight(2);
-        env.fill(this.color);
-        // if(this.body.isSleeping) {
-        //     env.fill();
-        // }
-
-        if (!chocolateBlocks) {
-            env.rect(0, 0, this.w, this.h);
-        }
-        else {
-            this.blockKind.drawChocolateBlocks(env);
-        }
-        env.pop();
-
-    }
-
-    this.can_be_placed = function () {
-        colliding_bodies = Matter.Query.region(engine.world.bodies, this.test_body.bounds);
-        return (colliding_bodies === undefined || colliding_bodies.length == 0)
-    }
-
-    this.checkMotion = function () {
-        //console.log('angle', this.body.angle);
-        //console.log('∆x', this.body.position.x - this.originalX);
-        //console.log('∆y', this.body.position.y - this.originalY);
-
-        var xMove = Math.abs(this.body.position.x - this.originalX) > 10;
-        var yMove = Math.abs(this.body.position.y - this.originalY) > 10;
-        var rotated = Math.abs(this.body.angle) > 0.70
-
-        return (xMove || yMove || rotated)
-    }
+    return (xMove || yMove || rotated)
+  }
 
 }
 
+module.exports = Block;
--- behavioral_experiments/static/js/blockKind.js
+++ behavioral_experiments/static/js/blockKind.js
@@ -1,111 +1,119 @@
-// BlockKinds are a type of block- of which several might be placed in the environment. To be concretely instantiated, a Block must be created that inherets its properties from a BlockKind
-// BlockKind also holds the information for displaying the menu item associated with that blockKind.
-function BlockKind(w,h,blockColor, blockName = ''){
-    // BlockKind width and height should be given in small integers. They are scaled in the block class.
+var config = require('./display_config.js');
+
+class BlockKind {
+
+  constructor(w, h, blockColor, blockName = '') {
+    // BlockKinds are a type of block- of which several might be placed
+    // in the environment. To be concretely instantiated, a Block must
+    // be created that inherets its properties from a BlockKind
+    // BlockKind also holds the information for displaying the menu item
+    // associated with that blockKind.  BlockKind width and height
+    // should be given in small integers. They are scaled in the block
+    // class.
+    this.x = undefined;
+    this.y = undefined;
     this.w = w;
     this.h = h;
     this.blockColor = blockColor;
     this.blockName = blockName;
-    
-    var x;
-    var y;
-
-    // removed: options variable- add in later when we need different properties with different block kinds
-
-    // show block scaled according to given ratio, in a given location
-    this.showMenuItem = function(env,x,y) {
-
-        this.x = x;
-        this.y = y;
-        env.push();
-        env.rectMode(env.CENTER);
-        env.fill(this.blockColor);
-        env.stroke([60,90,110]);
-        env.strokeWeight(2);
-        env.translate(x,y);
-        env.rect(0,0,this.w*sF,this.h*sF);
-        if (chocolateBlocks) {
-            this.drawChocolateBlocks(env);
-        }
-        env.pop();
-
+  }
+  
+  // show block scaled according to given ratio, in a given location
+  showMenuItem (env,x,y) {
+    this.x = x;
+    this.y = y;
+    env.push();
+    env.rectMode(env.CENTER);
+    env.fill(this.blockColor);
+    env.stroke([60,90,110]);
+    env.strokeWeight(2);
+    env.translate(x,y);
+    env.rect(0, 0, this.w * config.sF, this.h * config.sF);
+    if (config.chocolateBlocks) {
+      this.drawChocolateBlocks(env);
     }
+    env.pop();
+  }
 
-    this.drawChocolateBlocks = function(env) {
-        // draws unit squares on each block
-        nRow = this.w;
-        nCol = this.h;
-        i = -nRow/2 + 0.5;
-        while (i < nRow/2) {
-            j = -nCol/2 + 0.5;
-            while (j < nCol/2) { // draw one square
-                env.translate(sF*i, sF*j);
-                env.rect(0, 0, sF, sF);
-                env.translate(-sF*i, -sF*j);
-                j++;
-            }
-            i++;
-        }
+  drawChocolateBlocks (env) {
+    // draws unit squares on each block
+    var nRow = this.w;
+    var nCol = this.h;
+    var i = -nRow/2 + 0.5;
+    while (i < nRow/2) {
+      var j = -nCol/2 + 0.5;
+      while (j < nCol/2) { // draw one square
+        env.translate(config.sF*i, config.sF*j);
+        env.rect(0, 0, config.sF, config.sF);
+        env.translate(-config.sF*i, -config.sF*j);
+        j++;
+      }
+      i++;
     }
+  }
 
+  showGhost (env, mouseX, mouseY, rotated, discreteWorld,
+             disabledBlockPlacement = false, snapToGrid = true) {
+    if((mouseX > (config.sF*(this.w/2))) &&
+       (mouseX < config.canvasWidth-(config.sF*(this.w/2)))){
 
-    this.showGhost = function(env, mouseX, mouseY, rotated, disabledBlockPlacement = false, snapToGrid = true) {
+      if(snapToGrid){
+        var stim_scale = config.stim_scale;
+        var snappedX;
+        var x_index;
+        if (this.w%2 == 1) {
+          snappedX = (mouseX+stim_scale/2)%(stim_scale) < (stim_scale/2) ? mouseX - (mouseX%(stim_scale/2)) : mouseX - (mouseX%(stim_scale)) + (stim_scale/2);
+          x_index = snappedX/stim_scale - snappedX%stim_scale + 7 + 5;
+        } else {
+          snappedX = mouseX%(stim_scale) < (stim_scale/2) ? mouseX - mouseX%(stim_scale) : mouseX - mouseX%(stim_scale) + stim_scale;
+          x_index = snappedX/stim_scale - snappedX%stim_scale - this.w/2 - 5 + 5;
+        };
 
-            if((mouseX > (sF*(this.w/2))) && (mouseX < canvasWidth-(sF*(this.w/2)))){
+        mouseX = snappedX
 
-            if(snapToGrid){
-                if (this.w%2 == 1) {
-                    snappedX = (mouseX+stim_scale/2)%(stim_scale) < (stim_scale/2) ? mouseX - (mouseX%(stim_scale/2)) : mouseX - (mouseX%(stim_scale)) + (stim_scale/2);
-                    x_index = snappedX/stim_scale - snappedX%stim_scale + 7 + 5;
-                } else {
-                    snappedX = mouseX%(stim_scale) < (stim_scale/2) ? mouseX - mouseX%(stim_scale) : mouseX - mouseX%(stim_scale) + stim_scale;
-                    x_index = snappedX/stim_scale - snappedX%stim_scale - selectedBlockKind.w/2 - 5 + 5;
-                };
+        var y = Math.round(13 - (this.h/2) - ((mouseY+(stim_scale/2))/stim_scale)) + 2;
+        var rowFree = true;
+        while (rowFree && y>=0) {
+          y-=1;
+          var blockEnd = x_index + this.w
+          for (let x = x_index; x < blockEnd; x++) { // check if row directly beneath block are all free at height y
+            //console.log('checking:', y, x)
+            rowFree = rowFree && discreteWorld[x][y];
+          }
 
-                mouseX = snappedX
-
-                var y = Math.round(13 - (this.h/2) - ((mouseY+(stim_scale/2))/stim_scale)) + 2;
-                var rowFree = true;
-                while (rowFree && y>=0) {
-                    y-=1;
-                    var blockEnd = x_index+selectedBlockKind.w
-                    for (let x = x_index; x < blockEnd; x++) { // check if row directly beneath block are all free at height y
-                        //console.log('checking:', y, x)
-                        rowFree = rowFree && discreteWorld[x][y];
-                    }
-
-                }
-                y_index = y+1;
-                //console.log('y_index',y_index);
-                
-                // ADD SNAP TO Y
-                snappedY = (canvasHeight - floorHeight) - (stim_scale*(selectedBlockKind.h/2)) - (stim_scale*(y_index)) + stim_scale/2 + 6;
-
-                mouseY = snappedY;
-            }
-
-            env.push();
-            env.translate(mouseX, mouseY);
-            env.rectMode(env.CENTER);
-            env.stroke([200,200,255]);
-            //env.stroke([28,54,62,100]);
-            env.strokeWeight(2);
-            //fillColor = disabledBlockPlacement ? [100, 100, 100, 100] : [...this.blockColor];
-            //fillColor[3] = 130;
-            fillColor = env.color([28,54,220]);
-            fillColor.setAlpha(50);
-            env.fill(fillColor);
-            if(rotated){
-                env.rect(0,0,this.h*sF,this.w*sF);
-            } else {
-                env.rect(0,0,this.w*sF,this.h*sF);
-            }
-            if (chocolateBlocks) {
-                this.drawChocolateBlocks(env);
-            }
-            env.pop();
         }
+        var y_index = y+1;
+        
+        // ADD SNAP TO Y
+        mouseY = (
+          (config.canvasHeight - config.floorHeight) -
+            (config.stim_scale*(this.h/2)) -
+            (stim_scale*(y_index)) + stim_scale/2 + 6
+        );
+      }
 
+      env.push();
+      env.translate(mouseX, mouseY);
+      env.rectMode(env.CENTER);
+      env.stroke([200,200,255]);
+      //env.stroke([28,54,62,100]);
+      env.strokeWeight(2);
+      //fillColor = disabledBlockPlacement ? [100, 100, 100, 100] : [...this.blockColor];
+      //fillColor[3] = 130;
+      var fillColor = env.color([28,54,220]);
+      fillColor.setAlpha(50);
+      env.fill(fillColor);
+      if(rotated){
+        env.rect(0,0,this.h * config.sF,this.w * config.sF);
+      } else {
+        env.rect(0,0,this.w * config.sF,this.h * config.sF);
+      }
+      if (config.chocolateBlocks) {
+        this.drawChocolateBlocks(env);
+      }
+      env.pop();
     }
-
+  }
 }
+
+module.exports = BlockKind;
