161a602f573369a2d30b7a347c01704f25a39a50
Robert Hawkins <rxdh@stanford.edu>
2020-02-17 21:11:01 -0500

    class-ify boundary.js

--- behavioral_experiments/static/js/boundary.js
+++ behavioral_experiments/static/js/boundary.js
@@ -1,38 +1,44 @@
+var config = require('./display_config.js');
+var Matter = require('./matter.js');
+
 // Wrapper for Bodies.rectangle class to create boundaries/ walls. 
 // Also includes a show function which draws the box using P5.
+class Boundary {
 
-function Boundary(x, y, w, h){
-    
+  constructor(x, y, w, h){
     var options = {
-        isStatic: true, // static i.e. not affected by gravity
-        friction: 0.9,
-        frictionStatic: 2
-        //slop: 0,
-        //restitution: 0
-    }
+      isStatic: true, // static i.e. not affected by gravity
+      friction: 0.9,
+      frictionStatic: 2
+    };
     
     this.w = w;
     this.h = h;
 
-    this.body = Bodies.rectangle(x*worldScale,y*worldScale,w*worldScale,h*worldScale, options);
-    World.add(engine.world, this.body); 
+    this.body = Matter.Bodies.rectangle(
+      x * config.worldScale,
+      y * config.worldScale,
+      w * config.worldScale,
+      h * config.worldScale,
+      options
+    );
+  }
+  
+  // Display the boundary on the screen
+  show (env) {
+    var pos = this.body.position;
+    var angle = this.body.angle;
 
-    // Display the boundary on the screen
-    this.show = function(env) {
-
-        var pos = this.body.position;
-        var angle = this.body.angle;
-
-        env.push();
-        env.translate(pos.x/worldScale, pos.y/worldScale);
-        env.rectMode(env.CENTER);
-        env.rotate(angle);
-        env.stroke(220);
-        env.strokeWeight(2);
-        env.fill([28,54,62]);
-        env.rect(0,0,this.w,this.h);
-        env.pop();
-        showMarker(env)
-    }
-
+    env.push();
+    env.translate(pos.x / config.worldScale, pos.y / config.worldScale);
+    env.rectMode(env.CENTER);
+    env.rotate(angle);
+    env.stroke(220);
+    env.strokeWeight(2);
+    env.fill([28,54,62]);
+    env.rect(0, 0, this.w, this.h);
+    env.pop();
+  };
 }
+
+module.exports = Boundary;
