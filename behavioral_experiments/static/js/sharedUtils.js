512131fb91ada463464a0140c379e6ca202135e5
Robert Hawkins <rxdh@stanford.edu>
2020-04-15 22:58:08 -0700

    add utility function to generate k combinations

--- behavioral_experiments/static/js/sharedUtils.js
+++ behavioral_experiments/static/js/sharedUtils.js
@@ -49,6 +49,35 @@ var checkPreviousParticipant = function(workerId, callback) {
   );
 };
 
+const k_combinations = (set, k) => {
+  if (k > set.length || k <= 0) {
+    return []
+  }
+  
+  if (k == set.length) {
+    return [set]
+  }
+  
+  if (k == 1) {
+    return set.reduce((acc, cur) => [...acc, [cur]], [])
+  }
+  
+  let combs = [], tail_combs = []
+  
+  for (let i = 0; i <= set.length - k + 1; i++) {
+    tail_combs = k_combinations(set.slice(i + 1), k - 1)
+    for (let j = 0; j < tail_combs.length; j++) {
+      combs.push([set[i], ...tail_combs[j]])
+    }
+  }
+  
+  return combs
+}
+
+const combinations = set => {
+  return set.reduce((acc, cur, idx) => [...acc, ...k_combinations(set, idx + 1)], [])
+}
+
 var writeDataToCSV = function(game, _dataPoint) {
   var dataPoint = _.clone(_dataPoint);  
   var eventType = dataPoint.eventType;
@@ -262,6 +291,7 @@ var series = function makeSeries(lb,ub) {
 
 module.exports = {
   UUID,
+  k_combinations,
   checkPreviousParticipant,
   serveFile,
   handleDuplicate,
