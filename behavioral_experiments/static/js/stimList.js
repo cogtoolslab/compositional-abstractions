b6e20d72ed6d1d5d73cfc9620f574dae68fd6005
Cameron Holdaway <cameron.holdaway@gmail.com>
2020-04-17 16:17:53 -0700

    removed chunky T

--- behavioral_experiments/static/js/stimList.js
+++ behavioral_experiments/static/js/stimList.js
@@ -6,10 +6,10 @@ const _rawStimList = {
     { "x": 0, "y": 0, "width": 2, "height": 1 },
     { "x": 2, "y": 0, "width": 2, "height": 1 }],
 
-    T: [{ "x": 1, "y": 0, "width": 1, "height": 2 },
-    { "x": 2, "y": 0, "width": 1, "height": 2 },
-    { "x": 0, "y": 2, "width": 2, "height": 1 },
-    { "x": 2, "y": 2, "width": 2, "height": 1 }],
+    // T: [{ "x": 1, "y": 0, "width": 1, "height": 2 },
+    // { "x": 2, "y": 0, "width": 1, "height": 2 },
+    // { "x": 0, "y": 2, "width": 2, "height": 1 },
+    // { "x": 2, "y": 2, "width": 2, "height": 1 }],
 
     C: [{ "x": 1, "y": 0, "width": 2, "height": 1 },
     { "x": 1, "y": 1, "width": 1, "height": 2 },
