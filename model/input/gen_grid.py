a83ad703c6a578ed361d6153c93328d82a6239d7
Robert Hawkins <hawkrobe@gmail.com>
2021-05-04 10:41:57 -0700

    add participantNumber as command-line arg

--- model/input/gen_grid.py
+++ model/input/gen_grid.py
@@ -6,5 +6,6 @@ with open('grid.csv', 'w') as csv_file :
     writer = csv.writer(csv_file, delimiter=',')
     for alpha in [1, 4, 16, 64] :
         for beta in np.linspace(0, 1, 11) :
-            writer.writerow([chainNum, round(alpha, 1), round(beta, 1)])
-            chainNum = chainNum + 1
+            for participantNumber in range(1, 50) :
+                writer.writerow([chainNum, round(alpha, 1), round(beta, 1), participantNumber])
+                chainNum = chainNum + 1
