2b1b37c789e538aeb5fc76b68d7dfd14f2fd7246
Will McCarthy <mccarthywill@live.co.uk>
2021-05-09 16:35:28 -0700

    restructure data

--- model/input/gen_grid.py
+++ model/input/gen_grid.py
@@ -2,10 +2,10 @@ import csv
 import numpy as np
 
 chainNum = 0
-with open('grid.csv', 'w') as csv_file :
+with open('grid_49ppts.csv', 'w') as csv_file :
     writer = csv.writer(csv_file, delimiter=',')
     for alpha in [1, 4, 16, 64] :
         for beta in np.linspace(0, 1, 11) :
-            for participantNumber in range(1, 3) :
+            for participantNumber in range(1, 50) :
                 writer.writerow([chainNum, round(alpha, 1), round(beta, 1), participantNumber])
                 chainNum = chainNum + 1
